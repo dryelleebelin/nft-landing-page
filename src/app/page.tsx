1003bb276657be62614164049267c7c010fd49ef
Dryelle Ebelin <dryellesilva07@gmail.com>
2025-12-18 16:58:02 -0300

    feat(hero): add responsive hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/layout/Header";
+import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
     <main className="h-screen font-inter bg-slate-900 text-slate-300">
       <Header />
+      <Hero />
     </main>
   );
 }
